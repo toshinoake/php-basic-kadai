299456304bc67c689c622dd4bc447d17d6793025
toshinoake <135620828+toshinoake@users.noreply.github.com>
2023-09-02 22:08:06 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編_課題7</title>
+  </head>
+
+  <body>
+    <p>
+      <?php
+        //連想配列に値を代入する
+        $Array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        //連想配列の値を出力する
+        print_r($Array);
+      ?>
+    </p>
+  </body>
+</html>
